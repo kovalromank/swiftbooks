2cb2d261e991464c9151183458fa15e016384ae3
Roman Koval <rkoval2@uwo.ca>
2023-11-29 14:25:02 -0500

    add page title to book list create and view pages

--- web/src/app/book-lists/view/[id]/page.tsx
+++ web/src/app/book-lists/view/[id]/page.tsx
@@ -1,7 +1,14 @@
 import { FC } from "react";
+import { Metadata } from "next";
 
 import { ReturnBreadcrumb } from "@/components/return-breadcrumb";
 import { BookListView } from "@/components/book-list/view/book-list-view";
+
+export const generateMetadata = ({ params }: BookListsViewProps): Metadata => {
+  return {
+    title: `${params.id} | Swift Books`,
+  };
+};
 
 interface BookListsViewProps {
   params: { id: string };
