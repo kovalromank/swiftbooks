a202c600117e70c2543a4722fe7d2387b2f3cbf1
Roman Koval <rkoval2@uwo.ca>
2023-11-29 18:50:26 -0500

    replace popover in menu with modal

--- web/src/components/cart/cart-menu.tsx
+++ web/src/components/cart/cart-menu.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { FC, useState } from "react";
-import { ActionIcon, Popover, PopoverTarget, PopoverDropdown } from "@mantine/core";
+import { FC, useState, MouseEvent, useCallback } from "react";
+import { ActionIcon, Modal } from "@mantine/core";
 import { IconShoppingBag } from "@tabler/icons-react";
 
 import { Cart } from "./cart";
@@ -10,25 +10,31 @@ import classes from "./cart-menu.module.css";
 
 export const CartMenu: FC = () => {
   const [opened, setOpened] = useState(false);
+  const [offset, setOffset] = useState<{ x?: number; y?: number }>({ x: undefined, y: undefined });
+
+  const onCartClick = useCallback((event: MouseEvent<HTMLButtonElement>) => {
+    const { x, y, width, height } = event.currentTarget.getBoundingClientRect();
+    setOffset({ x: window.innerWidth - (x + width), y: y + height });
+    setOpened(true);
+  }, []);
 
   return (
-    <Popover
-      shadow="sm"
-      radius="md"
-      position="bottom-end"
-      trapFocus
-      classNames={{ dropdown: classes.dropdown }}
-      opened={opened}
-      onChange={setOpened}
-    >
-      <PopoverTarget>
-        <ActionIcon size="2.5rem" variant="subtle" color="black" onClick={() => setOpened(!opened)}>
-          <IconShoppingBag size="2.5rem" />
-        </ActionIcon>
-      </PopoverTarget>
-      <PopoverDropdown>
+    <>
+      <ActionIcon size="2.5rem" variant="subtle" color="black" onClick={onCartClick}>
+        <IconShoppingBag size="2.5rem" />
+      </ActionIcon>
+      <Modal
+        padding="1.25rem"
+        trapFocus
+        title={<span className={classes.header}>Your shopping cart</span>}
+        opened={opened}
+        onClose={() => setOpened(false)}
+        xOffset={offset.x}
+        yOffset={offset.y}
+        classNames={{ inner: classes.modalInner }}
+      >
         <Cart onClose={() => setOpened(false)} />
-      </PopoverDropdown>
-    </Popover>
+      </Modal>
+    </>
   );
 };
