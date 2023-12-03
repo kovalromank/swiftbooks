328c54f5380aa7bf12b860ce76d94b4369c2d6bb
Roman Koval <rkoval2@uwo.ca>
2023-12-03 14:50:42 -0500

    move formats to utils file

---
+++ web/src/utils.ts
@@ -0,0 +1,23 @@
+import { useMemo } from "react";
+
+export const useCurrencyFormat = () =>
+  useMemo(
+    () =>
+      new Intl.NumberFormat(undefined, {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2,
+      }),
+    [],
+  );
+
+export const useNumberFormat = () =>
+  useMemo(
+    () =>
+      new Intl.NumberFormat(undefined, {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2,
+      }),
+    [],
+  );
