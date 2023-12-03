f82a4f565723068cf1670546c157b531eaff1f97
Roman Koval <rkoval2@uwo.ca>
2023-12-02 22:27:56 -0500

    call login mutation in login component

--- web/src/components/auth/auth-box.tsx
+++ web/src/components/auth/auth-box.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from "react";
+import { FC, FormEvent, ReactNode } from "react";
 import { Button, Card } from "@mantine/core";
 import { IconBrandGoogleFilled } from "@tabler/icons-react";
 
@@ -10,15 +10,24 @@ import classes from "./login.module.css";
 export interface AuthBoxProps {
   variant?: "login" | "register";
   children?: ReactNode;
+  loading?: boolean;
+  onSubmit?: (event: FormEvent<HTMLFormElement>) => void;
+  onReset?: (event: FormEvent<HTMLFormElement>) => void;
 }
 
-export const AuthBox: FC<AuthBoxProps> = ({ variant = "login", children }) => {
+export const AuthBox: FC<AuthBoxProps> = ({
+  variant = "login",
+  loading,
+  children,
+  onSubmit,
+  onReset,
+}) => {
   return (
     <Card shadow="sm" padding="md" radius="md" withBorder className={classes.container}>
       <Header>{variant === "register" ? "Register" : "Login"}</Header>
-      <form className={classes.sectionContainer} onSubmit={(e) => e.preventDefault()}>
+      <form className={classes.sectionContainer} onSubmit={onSubmit} onReset={onReset}>
         <div className={classes.inputContainer}>{children}</div>
-        <Button type="submit" variant="filled">
+        <Button type="submit" variant="filled" loading={loading}>
           {variant === "register" ? "REGISTER" : "LOGIN"}
         </Button>
         <Hr />
