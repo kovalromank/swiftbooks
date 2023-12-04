36ae6bc46cc0014c38a8de92f8a099d94b6f9166
Roman Koval <rkoval2@uwo.ca>
2023-12-04 12:07:45 -0500

    update review component with visibility toggle

--- web/src/components/book-list/view/review.tsx
+++ web/src/components/book-list/view/review.tsx
@@ -1,44 +1,86 @@
-import { FC, useMemo } from "react";
-import { Card, Rating, Tooltip } from "@mantine/core";
+import { FC, useCallback, useEffect, useState } from "react";
+import { Card, Chip, Rating, Tooltip } from "@mantine/core";
 import humanizeDuration from "humanize-duration";
 import { DateTime } from "luxon";
+import { IconEyeOff } from "@tabler/icons-react";
 
 import { ApiReview } from "@/api/types";
 
 import classes from "./review.module.css";
+import { useAuth } from "@/components/auth/auth-context";
+import { useCurrentUser, useToggleReviewMutation } from "@/api/api";
 
-interface ReviewProps {
+interface DateProps {
+  addedAt: string;
+}
+
+const Date: FC<DateProps> = ({ addedAt }) => {
+  const [label, setLabel] = useState("");
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    let timeout: number;
+
+    const updateDate = () => {
+      const dateTime = DateTime.fromISO(addedAt);
+
+      if (!dateTime.isValid) {
+        return -1;
+      }
+
+      const humanizer = humanizeDuration.humanizer({
+        units: ["y", "d", "h", "m", "s"],
+        largest: 1,
+        round: true,
+      });
+
+      const diff = Math.abs(dateTime.diffNow("milliseconds").milliseconds);
+
+      setContent(humanizer(diff) + " ago");
+      setLabel(dateTime.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS));
+
+      return diff;
+    };
+
+    const update = () => {
+      const diff = updateDate();
+
+      if (diff !== -1 && diff < 60 * 1000) {
+        timeout = setTimeout(update, 1000) as unknown as number;
+      }
+    };
+
+    update();
+
+    return () => {
+      timeout && clearTimeout(timeout);
+    };
+  }, [addedAt]);
+
+  return (
+    <Tooltip color="gray" position="bottom" inline label={label} className={classes.tooltip}>
+      <span>{content}</span>
+    </Tooltip>
+  );
+};
+
+export interface ReviewProps {
   data: ApiReview;
 }
 
 export const Review: FC<ReviewProps> = ({ data }) => {
-  const date = useMemo(() => {
-    const dateTime = DateTime.fromISO(data.added_at);
+  const auth = useAuth();
+  const { data: currentUser, isSuccess } = useCurrentUser(auth.status.isAuthenticated);
+  const { mutate } = useToggleReviewMutation();
 
-    if (!dateTime.isValid) {
-      return "";
-    }
+  const canHide =
+    auth.status.isAuthenticated &&
+    isSuccess &&
+    (currentUser.status === "manager" || currentUser.status === "admin");
 
-    const humanizer = humanizeDuration.humanizer({
-      units: ["y", "d", "h", "m", "s"],
-      largest: 1,
-      round: true,
-    });
-
-    const formatted = humanizer(Math.abs(dateTime.diffNow("milliseconds").milliseconds));
-
-    return (
-      <Tooltip
-        color="gray"
-        position="bottom"
-        inline
-        label={dateTime.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS)}
-        className={classes.tooltip}
-      >
-        <span>{formatted + " ago"}</span>
-      </Tooltip>
-    );
-  }, [data.added_at]);
+  const onHideClick = useCallback(() => {
+    mutate(data.id);
+  }, [mutate, data.id]);
 
   return (
     <Card shadow="sm" padding="md" radius="md" withBorder>
@@ -46,9 +88,26 @@ export const Review: FC<ReviewProps> = ({ data }) => {
         <div className={classes.creator}>{data.username}</div>
         <Rating value={data.stars} count={5} readOnly />
         <div className={classes.spacer}></div>
-        <div className={classes.date}>{date}</div>
+        <div className={classes.date}>
+          <Date addedAt={data.added_at} />
+        </div>
       </div>
       <div className={classes.content}>{data.content}</div>
+      {canHide ? (
+        <div className={classes.hideContainer}>
+          <Chip
+            icon={<IconEyeOff />}
+            checked={data.hidden}
+            color="red"
+            variant="light"
+            radius="sm"
+            onChange={onHideClick}
+            classNames={{ label: classes.hide }}
+          >
+            {data.hidden ? "HIDDEN" : "VISIBLE"}
+          </Chip>
+        </div>
+      ) : null}
     </Card>
   );
 };
